de85c7eb8943e7d791d04a03c1771b2844da3827
Vinit ranjan <88725714+Vinitranjansah@users.noreply.github.com>
2023-10-09 11:49:57 +0530

    Update javas2.js

--- javas2.js
+++ javas2.js
@@ -7,12 +7,12 @@ document.write(`
    <option value="null">choose</option>
   <option value="1">Dr. Abhik Mukherjee</option>
   <option value="2">Dr. Apurba Sarkar</option>
-  <option value="3">Dr. Ashish Kumar Layek</option>
+  <option value="3">Mr. Ashish Kumar Layek</option>
   <option value="4">Dr. Asit Kumar Das</option>
   <option value="5">Dr. Biplab Kumar Sikdar</option>
   <option value="6">Dr. Jaya Sil</option>
   <option value="7">Dr. Malay Kule</option>
-  <option value="8">Dr. Manas Hira</option>
+  <option value="8">Mr. Manas Hira</option>
   <option value="9">Dr. Nirnay Ghosh</option>
   <option value="10">Dr. Samit Biswas</option>
   <option value="11">Dr. Sekhar Mandal</option>
